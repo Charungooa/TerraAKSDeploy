a913345bea1528296e89c3584a3fc425d0f20daf
Amit Charungoo <v-2amitc@hotmail.com>
2025-09-01 23:30:09 +0530

    updated the scale method to automatic

--- Dev/main.tf
+++ Dev/main.tf
@@ -56,6 +56,7 @@ resource "azurerm_kubernetes_cluster" "terra-aks" {
     vm_size         = "Standard_DS2_v2"
     os_disk_size_gb = 30
     type            = "VirtualMachineScaleSets"
+    auto_scaling_enabled = true
     min_count           = 1
     max_count           = 5
   }
